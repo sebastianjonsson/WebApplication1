c4faf282f57116e0f7f3feb69342e3396d994886
Sebbe <sebastian.jonsson93@hotmail.com>
2017-12-13 13:42:12 +0100

    hej

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Profil()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application  page.";
 
             return View();
         }
