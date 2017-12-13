b09ae65c105f9983599135130b251ea221eb81a5
Sebbe <sebastian.jonsson93@hotmail.com>
2017-12-13 13:38:23 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Startsida()
+        {
+            return View();
+        }
+
+        public ActionResult Profil()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Vänner()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult LoggaIn()
+        {
+            return View();
+        }
+
+        public ActionResult Registrera()
+        {
+            return View();
+        }
+
+    }
+}
